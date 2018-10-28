75417384d8103b698f987bc2742283ec92339755
youmanyousef <44135498+youmanyousef@users.noreply.github.com>
2018-10-28 10:25:25 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,232 +1,254 @@
-﻿var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-var jArray = ['1','2','3']
-//Configure logger settings.
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-    colorize:true
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = 'h!';
+var jArray = ['1','2','3'];
+var saying;
+client.on("ready", () => {
+  console.log("I am ready!");
+  client.user.setActivity('jays server getting bodied', { type: 'WATCHING' });
 });
-
-logger.level = 'debug';
-
-//Initialize the bot
-var bot = new Discord.Client({
-    token: auth.token,
-    autorun:true
-});
-
-bot.on('ready', function(evt){
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-    bot.setPresence( {game: {name:"jays server getting bodied", type:3}} );
-});
-
-bot.on('message', function(user, userID, channelID, message, evt){
-    //Bot needs to know if it will execute a command.
-    //Bot will listen for messages that start with an '!'.
-    if(message.substring(0, 1) == '!'){
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-
-        args = args.splice(1);
-        switch(cmd){
-            //help
-            case 'h':
-		bot.sendMessage({
-                    to: channelID,
-                    message: '**HAMDI BOT HELP** -- `!vincent` - _vincent rOAST_  || `!al` - _al rOAST_ || `!molly` - _molly rOAST_ || `!flag` - _the bosnian flag_ || `!jay` - _jay rOAST_ || `!hieu` - _hieu rOAST_ || `!hue` - _hue rOAST_ || `!justin` - _justin rOAST_ || `!ct` - _Counter-Jays Win_ || `!t` - _Jays Win_ || `!og` - _ORANGE!, og_'
-                });
-            break;
-	    //flag
-            case 'flag':
-		bot.sendMessage({
-                    to: channelID,
-                    message: 'https://i.imgur.com/xwpy5nP.png'
-                });
-            break;
-	    //al
-	    case 'al':
-		var randomJ = jArray[Math.floor(Math.random()*jArray.length)];
-		if (randomJ == jArray[0]) {
-			bot.sendMessage({
-                    		to: channelID,
-                    		message: 'Naw wanna fight bitch?'
-                	});
-		}
-		if (randomJ == jArray[1]) {
-			bot.sendMessage({
-                    		to: channelID,
-                    		message: 'Molly bestie aksjdhfasf 😂😭'
-                	});
-		}
-		if (randomJ == jArray[2]) {
-			bot.sendMessage({
-                    		to: channelID,
-                    		message: 'jk love you too'
-			});
-		}
-            break;
-	    //molly
-	    case 'molly':
-		bot.sendMessage({
-                    to: channelID,
-                    message: 'Al\'s my bestieee!'
-                });
-            break;
-	    //vincent
-	    case 'vincent':
-		var randomJ = jArray[Math.floor(Math.random()*jArray.length)];
-		if (randomJ == jArray[0]) {
-			bot.sendMessage({
-                    		to: channelID,
-                    		message: 'What you want nigga?'
-                	});
-		}
-		if (randomJ == jArray[1]) {
-			bot.sendMessage({
-                    		to: channelID,
-                    		message: 'PASS THE MINI\'S DAMN!!'
-                	});
-		}
-		if (randomJ == jArray[2]) {
-			bot.sendMessage({
-                    		to: channelID,
-                    		message: 'DAAAAAAAAAAAAAAAAAAAAAAAYUM O MY'
-			});
-		}
-            break;
-	    //jay
-            case 'jay':
-		var randomJ = jArray[Math.floor(Math.random()*jArray.length)];
-		if (randomJ == jArray[0]) {
-			bot.sendMessage({
-                    		to: channelID,
-                    		message: 'Bro, no joke i made him cry bruh! 2-0!!!'
-                	});
-		}
-		if (randomJ == jArray[1]) {
-			bot.sendMessage({
-                    		to: channelID,
-                    		message: 'Thats what you get for demoting me! Dont demote me below goons again or ill go 3-0'
-                	});
-		}
-		if (randomJ == jArray[2]) {
-			bot.sendMessage({
-                    		to: channelID,
-                    		message: 'I have a message and a appeal to my demote! First I wanna state that Im very upset with the recent demote that wasn\'t even specified for and all I had done is kick yousef under the tag of that him being rude and making up an argument and without my doubt my job is to prevent this with the previous role given an Baldi patrol that the job is too stop problems and took the correct and Human steps to prevent bad image on the server and would like this to be fixed -CHIEF EGG @BomberBanks @HiNoOnesHere Please tell me your response because im very upset with the Dog shit role I would love to contuine my job of stopping fights with the role made for it https://cdn.discordapp.com/attachments/442041450704928779/499056937996910612/video.mov'
-                	});
-		}
-            break;
-            //Justin
-	    case 'justin':
-				var randomJ = jArray[Math.floor(Math.random()*jArray.length)];
-				if (randomJ == jArray[0]){
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Justin got **BODIED** ok?'
-                });}
-				if (randomJ == jArray[1]){
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'If you dont stfu, ill body you like justin.'
-                });}
-				if (randomJ == jArray[2]){
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'You do know what I did to Justin, right?'
-                });}
-            break;
-            //Counter Jays
-            case 'ct':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Counter-Jays Win!'
-                });
-            break;
-            //ORANGE!
-	    case 'og':
-		var randomJ = jArray[Math.floor(Math.random()*jArray.length)];
-		if (randomJ == jArray[0]){
-                	bot.sendMessage({
-                	    	to: channelID,
-                    		message: 'oraAANGe!'
-               		});}
-		if (randomJ == jArray[1]){
-			bot.sendMessage({
-                	    	to: channelID,
-                    		message: ':large_orange_diamond::large_orange_diamond:'
-               		});
-		}
-		if (randomJ == jArray[2]){
-			bot.sendMessage({
-               	    	to: channelID,
-                    		message: 'OG! OG!'
-               		});
-		}
-            break;
-			
-            //Fortnite sayings
-	    case 'fortnite':
-		var randomJ = jArray[Math.floor(Math.random()*jArray.length)];
-		if (randomJ == jArray[0]){
-               		bot.sendMessage({
-                  	to: channelID,
-                   	message: 'Fuck fortnite, I just Play For Fun.'
-                	});
-		}
-		if (randomJ == jArray[1]){
-               		bot.sendMessage({
-                    	to: channelID,
-                    	message: 'Incorrect, Gary. I have 23* solos wins.'
-                	});
-		}
-		if (randomJ == jArray[2]){
-               		bot.sendMessage({
-                    	to: channelID,
-                    	message: 'Only reason i have 1W% is because there noclippers, and hackers.'
-                	});
-		}
-          break;
-            //Jays (t)
-            case 't':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Jays Win!'
-                });
-            break;
-	   case 'hue':
-                bot.sendMessage({
-                to: channelID,
-                message: 'I buy hue KFC🍗!'
-                });
-            break;
-            // hieu
-            case 'hieu':
-				var randomJ = jArray[Math.floor(Math.random()*jArray.length)];
-				if (randomJ == jArray[0])
-                bot.sendMessage({
-                    to: channelID,
-                    message: '1-0, bodied that nigga.'
-                });
-				if (randomJ == jArray[1])
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'I made that bitch tap out:boxing_glove:.'
-                });
-				if (randomJ == jArray[2])
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'What a gay ass, he tried to pull down my pants.'
-                });
-            break;
-            case 'frederica':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Ima marry her!'
-                });
-            break;
-        }
+ 
+client.on("message", (message) => {
+  //help
+  if (message.content.startsWith(prefix + "help")) {
+    //message.channel.send("**HAMDI BOT HELP** **h!** is the prefix -- `h!fortnite` - _fortnite corrections_ || `h!vincent` - _vincent rOAST_ || `h!al` - _al rOAST_ || `h!molly` - _molly rOAST_ || `h!flag` - _the bosnian flag_ || `h!jay` - _jay rOAST_ || `h!hieu` - _hieu rOAST_ || `h!hue` - _hue rOAST_ || `h!justin` - _justin rOAST_ || `h!ct` - _Counter-Jays Win_ || `h!t` - _Jays Win_ || `h!og` - _ORANGE!, og_");
+	const embed = {
+  "title": "Hamdija Bot Help!",
+  "color": 3447003,
+  "timestamp": "2018-10-28T16:55:15.116Z",
+  "thumbnail": {
+    "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+  },
+  "author": {
+    "name": "made by joseph (youmanyousef)",
+    "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
+  },
+  "fields": [
+    {
+      "name": "_`h!`_",
+      "value": "Is the prefix for all commands. the number in the description represents how many results you can get."
+    },
+    {
+      "name": "`h!flag`",
+      "value": "_The Bosnian Flag_",
+      "inline": true
+    },
+    {
+      "name": "`h!hamdi`",
+      "value": "_A shitty pic of Hamdija_",
+      "inline": true
+    },
+    {
+      "name": "`h!ms`",
+      "value": "_Gary's Mugshot_",
+      "inline": true
+    },
+    {
+      "name": "`h!al`",
+      "value": "_al rOAST_ (3)",
+      "inline": true
+    },
+    {
+      "name": "`h!molly`",
+      "value": "_molly rOAST_",
+      "inline": true
+    },
+    {
+      "name": "`h!vincent`",
+      "value": "_vincent rOAST_ (3)",
+      "inline": true
+    },
+    {
+      "name": "`h!justin`",
+      "value": "_justin rOAST_ (3)",
+      "inline": true
+    },
+    {
+      "name": "`h!hieu`",
+      "value": "_hieu rOAST_ (3)",
+      "inline": true
+    },
+    {
+      "name": "`h!hue`",
+      "value": "_hue rOAST_",
+      "inline": true
+    },
+    {
+      "name": "`h!ct`",
+      "value": "_Counter-Jays Win!_",
+      "inline": true
+    },
+    {
+      "name": "`h!t`",
+      "value": "_Jays Win!_",
+      "inline": true
+    },
+    {
+      "name": "`h!flag`",
+      "value": "_these last two_",
+      "inline": true
+    },
+    {
+      "name": "`h!og`",
+      "value": "_OG!_ (3)",
+      "inline": true
+    },
+    {
+      "name": "`h!fortnite`",
+      "value": "_Fortnite is Garbage._ (3)",
+      "inline": true
     }
+  ]
+};
+	message.channel.send({ embed });
+  }
+  //bosnia flag
+  if (message.content.startsWith(prefix + "flag")) {
+    message.channel.send("_Dobrodošli. Kako ste_?", {files: ["https://i.imgur.com/9Dvw7JV.jpg"]});
+  }
+  //sad
+  if (message.content.startsWith(prefix + "sad")) {
+    message.channel.send({files: ["https://i.imgur.com/9Dvw7JV.jpg"]});
+  }
+  //mugshot (ms)
+  if (message.content.startsWith(prefix + "ms")) {
+    message.channel.send({files: ["https://i.imgur.com/9Dvw7JV.jpg"]});
+  }
+  //hamdi
+  if (message.content.startsWith(prefix + "hamdi")) {
+    message.channel.send({files: ["https://i.imgur.com/9Dvw7JV.jpg"]});
+  }
+  //al rOAST
+  if (message.content.startsWith(prefix + "al")) {
+	var randomJ = jArray[Math.floor(Math.random()*jArray.length)];
+	if (randomJ == jArray[0]){
+		var saying = "Naw wanna fight bitch?";
+		message.channel.send(saying);
+	}
+	if (randomJ == jArray[1]){
+		var saying = "Molly bestie aksjdhfasf 😂😭";
+		message.channel.send(saying);
+	}
+	if (randomJ == jArray[2]){
+		var saying = "jk love you too";
+		message.channel.send(saying);
+	}
+  }
+  //molly rOAST
+  if (message.content.startsWith(prefix + "molly")) {
+    message.channel.send("Al\'s my bestieee!");
+  }
+  //vincent rOAST
+  if (message.content.startsWith(prefix + "vincent")) {
+    var randomJ = jArray[Math.floor(Math.random()*jArray.length)];
+	if (randomJ == jArray[0]){
+		var saying = "What you want nigga?";
+		message.channel.send(saying);
+	}
+	if (randomJ == jArray[1]){
+		var saying = "PASS THE MINI\'S DAMN!!";
+		message.channel.send(saying);
+	}
+	if (randomJ == jArray[2]){
+		var saying = "DAAAAAAAAAAAAAAAAAAAAAAAYUM O MY";
+		message.channel.send(saying);
+	}
+  }
+  //justin rOAST
+  if (message.content.startsWith(prefix + "justin")) { 
+    var randomJ = jArray[Math.floor(Math.random()*jArray.length)];
+	if (randomJ == jArray[0]){
+		var saying = "Justin got **BODIED** ok?";
+		message.channel.send(saying);
+	}
+	if (randomJ == jArray[1]){
+		var saying = "If you dont stfu, ill body you like justin.";
+		message.channel.send(saying);
+	}
+	if (randomJ == jArray[2]){
+		var saying = "You do know what I did to Justin, right?";
+		message.channel.send(saying);
+	}
+  }
+ 
+  //counter jays win
+  if (message.content.startsWith(prefix + "ct")) {
+    message.channel.send("Counter-Jays Win!");
+  }
+  //jays win
+  if (message.content.startsWith(prefix + "t")) {
+    message.channel.send("Jays Win!");
+  }
+  //og
+  if (message.content.startsWith(prefix + "og")) {
+    var randomJ = jArray[Math.floor(Math.random()*jArray.length)];
+	if (randomJ == jArray[0]){
+		var saying = "oraAANGe!";
+		message.channel.send(saying);
+	}
+	if (randomJ == jArray[1]){
+		var saying = ":large_orange_diamond::large_orange_diamond:";
+		message.channel.send(saying);
+	}
+	if (randomJ == jArray[2]){
+		var saying = "OG! OG!";
+		message.channel.send(saying);
+  }}
+  //fortnite
+  if (message.content.startsWith(prefix + "fortnite")) {
+    var randomJ = jArray[Math.floor(Math.random()*jArray.length)];
+	if (randomJ == jArray[0]){
+		var saying = "Fuck fortnite, I just Play For Fun.";
+		message.channel.send(saying);
+	}
+	if (randomJ == jArray[1]){
+		var saying = "Incorrect, Gary. I have 23* solo wins.";
+		message.channel.send(saying);
+	}
+	if (randomJ == jArray[2]){
+		var saying = "Only reason i have 1W% is because there noclippers, and hackers.";
+		message.channel.send(saying);
+	}
+  }
+  //hieu
+  if (message.content.startsWith(prefix + "hieu")) {
+    var randomJ = jArray[Math.floor(Math.random()*jArray.length)];
+	if (randomJ == jArray[0]){
+		var saying = "1-0, bodied that nigga.";
+		message.channel.send(saying);
+	}
+	if (randomJ == jArray[1]){
+		var saying = "I made that bitch tap out:boxing_glove:.";
+		message.channel.send(saying);
+	}
+	if (randomJ == jArray[2]){
+		var saying = "What a gay ass, he tried to pull down my pants.";
+		message.channel.send(saying);
+	}
+  }
+  //frederica
+  if (message.content.startsWith(prefix + "frederica")) {
+    message.channel.send("Ima marry her!");
+  }
+  //jay
+  if (message.content.startsWith(prefix + "jay")) {
+    var randomJ = jArray[Math.floor(Math.random()*jArray.length)];
+	if (randomJ == jArray[0]){
+		var saying = "Bro, no joke i made him cry bruh! 2-0!!!";
+		message.channel.send(saying);
+	}
+	if (randomJ == jArray[1]){
+		var saying = "I made that bitch tap out:boxing_glove:.";
+		message.channel.send(saying);
+	}
+	if (randomJ == jArray[2]){
+		var saying = "What a gay ass, he tried to pull down my pants.";
+		message.channel.send(saying);
+	}
+  }
+  //hue
+  if (message.content.startsWith(prefix + "hue")) {
+    message.channel.send("I buy hue KFC🍗!");
+  }
 });
+client.login(BOT_TOKEN);
